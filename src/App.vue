aa91b1806c55fc901887011768a0abb5cc813a22
Billy Ponto <bpgponto@gmail.com>
2022-04-07 13:36:41 +0800

    Reaction Time Game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer Game</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @result="endGame" />
+  <Results v-if="showResults" :scoreResults="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Results
+  },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+      //console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score = reactionTime //reactionTime took from Block.vue $emit
+      this.isPlaying = false
+      this.showResults = true
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+*{
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+#app {
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button{
+  background: #2e4cfa;
+  padding: 16px 28px;
+  border-radius: 8px;
+  color: white;
+  font-size: 18px;
+  font-weight: 500;
+  border: 0;
+  cursor: pointer;
+  transition: all .3s;
+}
+
+button[disabled]{
+  opacity: 0.4;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Reaction Time: {{ scoreResults }} </p>
+  <h4>{{ rank }}</h4>
+</template>
+
+<script>
+export default {
+    props: ['scoreResults'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.scoreResults < 250){
+            this.rank = 'Ninja Fingers'
+        }else if(this.scoreResults < 400){
+            this.rank = 'Rapid Reflexes'
+        }else{
+            this.rank = 'Turtle Man!'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
